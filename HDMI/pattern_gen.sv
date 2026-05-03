1276f9a93e7589118f1e2def96596cab737ba0ef
creepybloodykitten <nikita2005420054@gmail.com>
2026-05-03 09:22:33 +0700

    minor refactor

--- HDMI/pattern_gen.sv
+++ HDMI/pattern_gen.sv
@@ -1,48 +1,41 @@
 module pattern_gen (
-    input  logic clk,       // Тактовая частота 25 МГц (от PLL)
-    input  logic rst,       // Сигнал сброса (от кнопки)
-    input  logic [9:0] x,   // Текущая координата X (от vga_generator)
-    input  logic [9:0] y,   // Текущая координата Y (от vga_generator)
-    input  logic de,        // Флаг активного видео (Data Enable)
-	 input  logic[3:0] keys,
-    output logic [7:0] r,   // Выход цвета Red
-    output logic [7:0] g,   // Выход цвета Green
-    output logic [7:0] b    // Выход цвета Blue
+    input  logic clk,       // clk from pll
+    input  logic rst,       
+    input  logic [9:0] x,   // current pos X (от vga_generator)
+    input  logic [9:0] y,   // current pos Y (от vga_generator)
+    input  logic de,        // Data Enable
+	input  logic[3:0] keys,
+    output logic [7:0] r,   
+    output logic [7:0] g,   
+    output logic [7:0] b    
 );
 
-    // ==========================================
     // random number
-    // ==========================================
     logic [7:0] lfsr;
     
     always_ff @(posedge clk or posedge rst) begin
         if (rst) 
-            lfsr <= 8'hAA; // Стартовое значение (не должно быть нулем!)
+            lfsr <= 8'hAA; // start value
         else 
-            // Сдвиг влево и XOR для обратной связи
             lfsr <= {lfsr[6:0], lfsr[7] ^ lfsr[5] ^ lfsr[4] ^ lfsr[3]}; 
     end
 
-    // ==========================================
-    // positions
-    // ==========================================
-    logic frame_tick;
-    // Генерируем короткий импульс 1 раз за кадр (когда луч уходит в невидимую зону)
-    assign frame_tick = (x == 10'd639 && y == 10'd479);
 
-//    logic [9:0] block_x;
-//    logic [9:0] block_y;
+    // positions
+    logic frame_tick;
+    // generate frame tick at the end of each frame (when we are at the last pixel)
+    assign frame_tick = (x == 10'd639 && y == 10'd479);
 	 
 	 
-	 localparam BLOCK_WIDTH  = 10'd52; 
+	localparam BLOCK_WIDTH  = 10'd52; 
     localparam BLOCK_HEIGHT = 10'd13; 
 	 
-	 	 //capture zone
-	 localparam HIT_Y_START = 10'd400; 
+	//capture zone
+	localparam HIT_Y_START = 10'd400; 
     localparam HIT_Y_END   = 10'd419; 
 	 
-	 localparam MAX_BLOCKS   = 10; //max quantity of blocks on screen
-	 logic [9:0] block_y[0:MAX_BLOCKS-1]; // hight of every block
+	localparam MAX_BLOCKS   = 10; //max quantity of blocks on screen
+	logic [9:0] block_y[0:MAX_BLOCKS-1]; // hight of every block
     logic [1:0] block_lane[0:MAX_BLOCKS-1]; // in what colomn
     logic       block_visible [0:MAX_BLOCKS-1]; // 1 - block in procees to catch, 0 - caught/free
 	 
@@ -51,7 +44,7 @@ module pattern_gen (
     logic [3:0] keys_prev;     
 	 
 	 //logic to find block with visible =0 to regenerate
-	 logic[3:0] free_idx;
+	logic[3:0] free_idx;
     logic      has_free;
     always_comb begin
         free_idx = 4'd0;
@@ -69,52 +62,48 @@ module pattern_gen (
 	 assign keys_pulse = keys & ~keys_prev;
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-				for (int i=0; i<MAX_BLOCKS; i++) begin
+			for (int i=0; i<MAX_BLOCKS; i++) begin
                 block_visible[i] <= 1'b0;
                 block_y[i]       <= 10'd0;
                 block_lane[i]    <= 2'd0;
             end
-				for (int i=0; i<4; i++) flash[i] <= 5'd0;
+			for (int i=0; i<4; i++) flash[i] <= 5'd0;
             spawn_timer <= 0;
             keys_prev   <= 4'd0;
-            //block_y <= 10'd0;
-            //block_x <= 10'd221; // По умолчанию ставим в крайнюю левую полосу
         end else begin
-            // Если блок улетел за нижний край экрана
-				keys_prev <= keys;
-				
-				if (frame_tick) begin
-					 for(int j=0; j<4; j++) begin
+            keys_prev <= keys;
+            if (frame_tick) begin
+                for(int j=0; j<4; j++) begin
                     if (flash[j] > 0) flash[j] <= flash[j] - 5'd1;
                 end
-					 spawn_timer <= spawn_timer + 6'd1; //every 2 sec
-					 if (spawn_timer >= 6'd30) begin
+                spawn_timer <= spawn_timer + 6'd1; //every 2 sec
+                if (spawn_timer >= 6'd30) begin
                     spawn_timer <= 0;
                     if (has_free) begin
                         block_visible[free_idx] <= 1'b1;
                         block_y[free_idx]       <= 10'd0;
-                        block_lane[free_idx]    <= lfsr[1:0]; // Случайная колонка
+                        block_lane[free_idx]    <= lfsr[1:0]; // random coloumn for new block
                     end
                 end
-					 
-					 for (int i=0; i<MAX_BLOCKS; i++) begin
+                    
+                for (int i=0; i<MAX_BLOCKS; i++) begin
                     if (block_visible[i]) begin
                         if (block_y[i] >= 10'd480) 
-                            block_visible[i] <= 1'b0; // Блок улетел за экран (Miss)
+                            block_visible[i] <= 1'b0; // block is out of screen
                         else 
                             block_y[i] <= block_y[i] + 10'd3;
                     end
                 end
-				end
+            end
 					 
-				for (int i=0; i<MAX_BLOCKS; i++) begin
+			for (int i=0; i<MAX_BLOCKS; i++) begin
                 if (block_visible[i]) begin
-                    // Если нажата кнопка в колонке блока, и блок внутри зоны захвата
+                    // if we have press in current lane and block in hit zone by Y - catch block
                     if (keys_pulse[block_lane[i]] && 
                        (block_y[i] + BLOCK_HEIGHT >= HIT_Y_START) && 
                        (block_y[i] <= HIT_Y_END)) begin
                         
-                        block_visible[i]     <= 1'b0;  // Прячем пойманный блок
+                        block_visible[i]     <= 1'b0;  // hide block (caught)
                         flash[block_lane[i]] <= 5'd31; 
                     end
                 end
@@ -127,32 +116,26 @@ module pattern_gen (
         bloom_out = 8'h00;
         
         for (int i = 0; i < 4; i++) begin
-            // Если в этой колонке активна вспышка и текущий X в этой колонке
+            //if flash active and we are in rec zone
             if (flash[i] > 0 && in_rec_x[i]) begin
                 
-                // Проверяем, что мы выше нижней границы хит-зоны
+                // check if we are in hit zone by Y
                 if (y <= HIT_Y_END) begin
                     int intensity;
                     int base_val;
                     int dist_y;
                     
-                    // 1. Базовая яркость от таймера (от 0 до 248)
+                    // base value of bloom effect depends on flash timer, when flash = 31 -> base_val = 248, when flash = 1 -> base_val = 8
                     base_val = int'(flash[i]) * 8;
                     
-                    // 2. Расстояние вверх от хит-зоны
-                    // В самой хит-зоне (y около 410) dist_y будет около 0.
-                    // На самом верху экрана (y = 0) dist_y будет около 410.
+                    // distance from current pixel to hit zone, when we are in hit zone -> dist_y = 0
                     dist_y = int'(HIT_Y_END) - int'(y);
                     
-                    // 3. Формула столба:
-                    // Берем базовую яркость и вычитаем дистанцию по Y.
-                    // Коэффициент перед dist_y (например, 1 или 2) определяет высоту столба.
-                    // Если (dist_y * 1) -> столб высокий (на весь экран).
-                    // Если (dist_y * 3) -> столб короткий (быстро тускнеет вверх).
+                    // intensity of bloom effect decreases as we move away from hit zone
                     intensity = base_val - (dist_y * 2); 
                     
                     if (intensity > 0) begin
-                        // Сложение с насыщением
+                        // add bloom effect to current pixel
                         if (int'(bloom_out) + intensity > 255) bloom_out = 8'hFF;
                         else bloom_out = bloom_out + intensity[7:0];
                     end
@@ -160,62 +143,26 @@ module pattern_gen (
             end
         end
     end
-//	 always_comb begin
-//        bloom_out = 8'h00;
-//        
-//        for (int i = 0; i < 4; i++) begin
-//            if (flash[i] > 0) begin
-//					 int centerX, centerY;
-//                int dx, dy, distance, intensity; 
-//                // Центр колонки по X
-//                centerX = 215 + (i * 53) + 26;
-//                // Центр хит-зоны по Y
-//                centerY = (HIT_Y_START + HIT_Y_END) / 2;
-//                
-//                // Считаем примерное расстояние (Манхэттенское расстояние для экономии ресурсов)
-//                // dist = |dx| + |dy|
-//                dx = (x > centerX) ? (x - centerX) : (centerX - x);
-//                dy = (y > centerY) ? (y - centerY) : (centerY - y);
-//					 
-//                distance  = dx + dy;
-//
-//                // Вычисляем яркость в этой точке:
-//                // Базовая яркость зависит от таймера (flash * 8)
-//                // И уменьшается с расстоянием (dist * 2)
-//                intensity = (flash[i] * 10) - (distance  * 4);
-//                
-//                if (intensity > 0) begin
-//                    // Если интенсивность положительная, добавляем её к результату
-//                    // Используем сложение с насыщением (чтобы не превысить 255)
-//                    if (bloom_out + intensity > 255) bloom_out = 8'hFF;
-//                    else bloom_out = bloom_out + intensity[7:0];
-//                end
-//            end
-//        end
-//    end
 
-    // ==========================================
-    // 3. ЛОГИКА ОТРИСОВКИ СЛОЕВ (ГРАФИКА)
-    // ==========================================
+
+    //draw logic
     logic in_hit_zone_y;
     logic draw_line_left;
     logic draw_line_right;
     logic draw_zone;
     
-    logic [23:0] rgb_out; // Внутренний провод для сборки 24-битного цвета
+    logic [23:0] rgb_out; 
                        
-    // Флаг 2: Левая белая разделительная граница (ширина 2 пикселя)
+    // border lines
     assign draw_line_left  = (x == 10'd213) || (x == 10'd214);
-    
-    // Флаг 3: Правая белая разделительная граница (ширина 2 пикселя)
     assign draw_line_right = (x == 10'd426) || (x == 10'd427);
     
-    // Флаг 4: Игровой стакан (пространство между линиями)
+    // main game zone
     assign draw_zone = (x >= 10'd215) && (x < 10'd426);
 	 
-	 assign in_hit_zone_y   = (y >= HIT_Y_START) && (y <= HIT_Y_END);
+	assign in_hit_zone_y   = (y >= HIT_Y_START) && (y <= HIT_Y_END);
 
-	 logic draw_block_any;
+	logic draw_block_any;
     always_comb begin
         draw_block_any = 1'b0;
         for (int i = 0; i < MAX_BLOCKS; i++) begin
@@ -236,13 +183,13 @@ module pattern_gen (
         end
     end
 	 
-	 //user blocks in bottom
-	 localparam REC_Y_START = 10'd420; 
+	//user blocks in bottom
+	localparam REC_Y_START = 10'd420; 
     localparam REC_Y_END   = 10'd479; 
-	 logic in_rec_y;
+	logic in_rec_y;
     assign in_rec_y = (y >= REC_Y_START) && (y <= REC_Y_END);
 	 
-    logic [3:0] in_rec_x; // Определяем, в какой колонке находится пиксель
+    logic [3:0] in_rec_x; // understand in what colomn we are
     assign in_rec_x[0] = (x >= 10'd215) && (x <= 10'd266);
     assign in_rec_x[1] = (x >= 10'd268) && (x <= 10'd319);
     assign in_rec_x[2] = (x >= 10'd321) && (x <= 10'd372);
@@ -265,7 +212,7 @@ module pattern_gen (
                 rgb_out = 24'hFFFFFF; // white line of borders
 					 
 				else if (bloom_out > 200) begin 
-                // В самом центре вспышка почти белая
+                //in center of flash make white color
                 rgb_out = 24'hFFFFFF;
             end
 
@@ -275,7 +222,7 @@ module pattern_gen (
 				else begin
                 logic [7:0] base_r, base_g, base_b;
                 
-                // Определяем базовый цвет пикселя (кнопки, хит-зона или фон)
+                // base colors
                 if (fill_rec_0) {base_r, base_g, base_b} = keys[0] ? 24'hFFFFFF : 24'hFF8800;
                 else if (fill_rec_1) {base_r, base_g, base_b} = keys[1] ? 24'hFFFFFF : 24'hFF8800;
                 else if (fill_rec_2) {base_r, base_g, base_b} = keys[2] ? 24'hFFFFFF : 24'hFF8800;
@@ -284,37 +231,15 @@ module pattern_gen (
                 else if (draw_zone) {base_r, base_g, base_b} = 24'h222222;
                 else {base_r, base_g, base_b} = 24'h000000;
 
-                // ДОБАВЛЯЕМ СВЕЧЕНИЕ К БАЗОВОМУ ЦВЕТУ
-                // Это создает эффект "освещения" фоновых объектов вспышкой
+
+                // add effeect bloom
                 rgb_out[23:16] = (base_r + bloom_out > 255) ? 8'hFF : base_r + bloom_out;
                 rgb_out[15:8]  = (base_g + bloom_out > 255) ? 8'hFF : base_g + bloom_out;
                 rgb_out[7:0]   = (base_b + bloom_out > 255) ? 8'hFF : base_b + bloom_out;
             end
-					 
-				//hit zone
-//				else if (in_hit_zone_y && in_rec_x[0] && flash[0] > 0) rgb_out = 24'h33FF33;
-//            else if (in_hit_zone_y && in_rec_x[1] && flash[1] > 0) rgb_out = 24'h33FF33;
-//            else if (in_hit_zone_y && in_rec_x[2] && flash[2] > 0) rgb_out = 24'h33FF33;
-//            else if (in_hit_zone_y && in_rec_x[3] && flash[3] > 0) rgb_out = 24'h33FF33;
-//                
-//					 
-//            else if (in_hit_zone_y && draw_zone) 
-//                rgb_out = 24'h445566; 
-//				
-//				else if (fill_rec_0) rgb_out = keys[0] ? 24'hFFFFFF : 24'hFF8800; // FF8800 = orange
-//            else if (fill_rec_1) rgb_out = keys[1] ? 24'hFFFFFF : 24'hFF8800;
-//            else if (fill_rec_2) rgb_out = keys[2] ? 24'hFFFFFF : 24'hFF8800;
-//            else if (fill_rec_3) rgb_out = keys[3] ? 24'hFFFFFF : 24'hFF8800;
-// 
-//                
-//            else if (draw_zone)
-//                rgb_out = 24'h222222; //  main background
-//                
-//            else
-//                rgb_out = 24'h000000; 
         end
     end
-    // Разбиваем готовый 24-битный цвет на три 8-битных канала
+    // decompose 24-bit color to RGB channels
     assign r = rgb_out[23:16];
     assign g = rgb_out[15:8];
     assign b = rgb_out[7:0];
